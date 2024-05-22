78df1c40f0ac145ce93bcf874c6b9e1305238fd4
kaushik chaudhary <kaushik.chaudhary@projectkmt.com>
2024-05-22 23:24:26 +0530

    BAsic section of nodejs completed

--- nodeBasic/app.js
+++ nodeBasic/app.js
@@ -1,23 +1,8 @@
 const http = require('http');
+const request = require('./routes');
 
-function rqListener(req, res) {
-    const url = req.url;
-    console.log('req.url: ', req.url);
-    if (url === '/hi') {
-        res.setHeader('Content-Type', 'text-html');
-        res.write('<head><title>My First Page</title></head>');
-        res.write(`<body><form action="/message" style="text-align:center;color:orange"><input type="text" name="enter somthing"><button type="submit">submit</button></form>`);
-        return res.end();
-    }
+const server = http.createServer(request.Handler);
 
-    res.setHeader('Content-Type', 'text-html');
-    res.write('<head><title>My First Page</title></head>');
-    res.write(`<body><h1 style="text-align:center;color:orange">Hello From my node js server</h1></body>`);
-    res.end();
-
-    // process.exit(); // close process which we never do because we never want to our servert to close
-};
-
-const server = http.createServer(rqListener);
+console.log(request.someTexts);
 
 server.listen(3000);
---
+++ nodeBasic/routes.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const requestHnadler = (req, res) => {
+    const url = req.url;
+    if (url === '/hi') {
+        res.setHeader('Content-Type', 'text-html');
+        res.write('<head><title>My First Page</title></head>');
+        res.write(`<body><form action="/message" method="POST"><input type="text" name="enter somthing"><button type="submit">submit</button></form>`);
+        return res.end();
+    }
+
+    if (url === '/message' && method === "POST") {
+        const body = [];
+        req.on('data', (chank) => {
+            console.log('chank::', chank);
+            body.push(chank);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log('parsedBody: ', parsedBody);
+            fs.writeFile('message.txt', message);
+            res.statusCode = 302,
+                res.setHeader('Location', '/')
+            return res.end();
+        });
+    }
+
+    res.setHeader('Content-Type', 'text-html');
+    res.write('<head><title>My First Page</title></head>');
+    res.write(`<body><h1 style="text-align:center;color:orange">Hello From my node js server</h1></body>`);
+    res.end();
+
+}
+
+
+module.exports = {
+    Handler: requestHnadler,
+    someTexts: 'Hard coded Text here!'
+};
